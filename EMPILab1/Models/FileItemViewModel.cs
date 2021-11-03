8ba49b2e0d76e33a11afd05ab4ec239f49a36101
StasKrivosheya <stask49@gmail.com>
2021-11-03 16:06:17 +0200

    Added ability to load file from storage

---
+++ EMPILab1/Models/FileItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Mvvm;
+
+namespace EMPILab1.Models
+{
+    public class FileItemViewModel : BindableBase
+    {
+        #region -- Public properties --
+
+        private string _fileName;
+        public string FileName
+        {
+            get => _fileName;
+            set => SetProperty(ref _fileName, value);
+        }
+
+        private string _fileContent;
+        public string FileContent
+        {
+            get => _fileContent;
+            set => SetProperty(ref _fileContent, value);
+        }
+
+        #endregion
+    }
+}
--- EMPILab1/ViewModels/MainPageViewModel.cs
+++ EMPILab1/ViewModels/MainPageViewModel.cs
@@ -1,40 +1,73 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
 using EMPILab1.Models;
+using Prism.Commands;
 using Prism.Navigation;
+using Xamarin.Essentials;
 
 namespace EMPILab1.ViewModels
 {
     public class MainPageViewModel : BaseViewModel
     {
-        public MainPageViewModel(INavigationService navigationService)
+        public MainPageViewModel(
+            INavigationService navigationService)
             : base(navigationService)
         {
-            AreaData = new ObservableCollection<TestModel>()
-            {
-                new TestModel("Jan", 50),
-                new TestModel("Feb", 70),
-                new TestModel("Mar", 65),
-                new TestModel("Apr", 57),
-                new TestModel("May", 48),
-            };
-
-            ColumnData = new ObservableCollection<TestModel>()
-            {
-                new TestModel("Jan", 20),
-                new TestModel("Feb", 30),
-                new TestModel("Mar", 25),
-                new TestModel("Apr", 47),
-                new TestModel("May", 38),
-            };
         }
 
-        public ObservableCollection<TestModel> AreaData { get; set; }
+        #region -- Public properties --
 
-        public ObservableCollection<TestModel> ColumnData { get; set; }
+        private FileItemViewModel _selectedFile;
+        public FileItemViewModel SelectedFile
+        {
+            get => _selectedFile;
+            set => SetProperty(ref _selectedFile, value);
+        }
+
+        private bool _isFileSelected;
+        public bool IsFileSelected
+        {
+            get => _isFileSelected;
+            set => SetProperty(ref _isFileSelected, value);
+        }
+
+        private ICommand _LoadFileCommand;
+        public ICommand LoadFileCommand => _LoadFileCommand ??= new DelegateCommand(async () => await OnLoadFileCommandAync());
+
+        #endregion
+
+        #region -- Overrides --
 
         public override void Initialize(INavigationParameters parameters)
         {
             base.Initialize(parameters);
         }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private async Task OnLoadFileCommandAync()
+        {
+            var options = new PickOptions
+            {
+                PickerTitle = "Выберите .dat или .txt файл",
+            };
+
+            var pickedFile = await FilePicker.PickAsync(options);
+
+            if (pickedFile != null)
+            {
+                SelectedFile = new FileItemViewModel
+                {
+                    FileName = pickedFile.FileName,
+                    FileContent = System.IO.File.ReadAllText(pickedFile.FullPath),
+                };
+
+                IsFileSelected = true;
+            }
+        }
+
+        #endregion
     }
 }
